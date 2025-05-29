ce41198b5441f4070ad102e82e50ff90efcd4d8f
NicholasM <40876002+Minutemini46@users.noreply.github.com>
2025-05-28 17:17:35 -0700

    Added Display Text and Oil Barrels
    - Added display text at top of screen to show stats
    - Oil now randomly generates around oil field
    - Collecting all oil barrels will move onto next level

--- IceMan/Actor.cpp
+++ IceMan/Actor.cpp
@@ -13,6 +13,16 @@ StudentWorld* Actor::getWorld()
 	return world_;
 }
 
+bool Actor::isAlive()
+{
+	return alive_;
+}
+
+void Actor::setAlive(bool state)
+{
+	alive_ = state;
+}
+
 // *************************************
 // ************** ICEMAN ***************
 // *************************************
@@ -24,29 +34,37 @@ void Iceman::init()
 
 void Iceman::tick()
 {	
-	int key;
-	if (getWorld()->getKey(key) == true)
+	if (hp_ == 0)
+		return;
+	else
 	{
-		switch (key)
+		int key;
+		if (getWorld()->getKey(key) == true)
 		{
+			switch (key)
+			{
 			case KEY_PRESS_UP:
-				setDirection(up);
-				if(getY() < VIEW_HEIGHT - 4)
+				if(getDirection() != up)
+					setDirection(up);
+				else if (getY() < VIEW_HEIGHT - 4)
 					moveTo(getX(), getY() + 1);
 				break;
 			case KEY_PRESS_DOWN:
-				setDirection(down);
-				if (getY() > 0)
+				if (getDirection() != down)
+					setDirection(down);
+				else if (getY() > 0)
 					moveTo(getX(), getY() - 1);
 				break;
 			case KEY_PRESS_RIGHT:
-				setDirection(right);
-				if(getX() < VIEW_WIDTH - 4)
+				if (getDirection() != right)
+					setDirection(right);
+				else if (getX() < VIEW_WIDTH - 4)
 					moveTo(getX() + 1, getY());
 				break;
 			case KEY_PRESS_LEFT:
-				setDirection(left);
-				if (getX() > 0)
+				if (getDirection() != left)
+					setDirection(left);
+				else if (getX() > 0)
 					moveTo(getX() - 1, getY());
 				break;
 			case KEY_PRESS_SPACE:
@@ -57,12 +75,15 @@ void Iceman::tick()
 			case 'z':
 				break;
 			case KEY_PRESS_ESCAPE:
+				abortLevel();
 				break;
 			default:
 				break;
+			}
+			// Mine Ice
+			if(getWorld()->deleteIce(getX(), getX() + 3, getY(), getY() + 3)) // try to dig 4x4 area of ice
+				getWorld()->playSound(SOUND_DIG);
 		}
-		// Mine Ice
-		getWorld()->deleteIce(getX(), getX() + 3, getY(), getY() + 3);
 	}
 }
 
@@ -71,9 +92,20 @@ void Iceman::annoyed()
 
 }
 
+void Iceman::squirtWater()
+{
+	water_--;
+}
+
+void Iceman::abortLevel()
+{
+	hp_ = 0;
+}
+
+
 void Iceman::addWater()
 {
-	water_++;
+	water_ += 5;
 }
 
 void Iceman::addSonar()
@@ -84,6 +116,27 @@ void Iceman::addSonar()
 void Iceman::addGold()
 {
 	gold_++;
+}
+
+
+int Iceman::getWater()
+{
+	return water_;
+}
+
+int Iceman::getGold()
+{
+	return gold_;
+}
+
+int Iceman::getSonar()
+{
+	return sonar_;
+}
+
+int Iceman::getHealth()
+{
+	return hp_;
 }
 
 
@@ -105,4 +158,57 @@ void Ice::tick()
 void Ice::annoyed()
 {
 
+}
+
+
+
+
+
+// *************************************
+// *********** COLLECTABLE *************
+// *************************************
+
+void Collectable::tick()
+{
+	if (isAlive())
+	{
+		if (!isVisible() && icemanWithinDist(4))
+		{
+			setVisible(true);
+			return;
+		}
+		else if (icemanWithinDist(3))
+		{
+			activate();
+		}
+	}
+}
+
+bool Collectable::icemanWithinDist(int numUnits)
+{
+	Actor* iceman = getWorld()->getIceman();
+
+	int icemanX = iceman->getX();
+	int icemanY = iceman->getY();
+
+	if (abs(getX() - icemanX) <= numUnits && abs(getY() - icemanY) <= numUnits)
+		return true;
+
+	return false;
+}
+
+// *************************************
+// **************** OIL ****************
+// *************************************
+
+void OilBarrel::init()
+{
+	setVisible(false);
+}
+
+void OilBarrel::activate()
+{
+	setAlive(false);
+	getWorld()->playSound(SOUND_FOUND_OIL);
+	getWorld()->increaseScore(1000);
 }
